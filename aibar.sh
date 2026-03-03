1d2642dfc0f59bb4b808dbd71619acfe37865c2b
Francesco Rolando <ogekuri@gmail.com>
2026-03-03 18:22:15 +0100

    new: Add requirements.txt file.

---
+++ aibar.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# VERSION: 0.8.0
+# AUTHORS: Ogekuri
+
+now=$(date '+%Y-%m-%d_%H-%M-%S')
+
+# 1. Get the full absolute path of the file (resolving symbolic links)
+FULL_PATH=$(readlink -f "$0")
+
+# 2. Extract the directory (the path without the filename)
+SCRIPT_PATH=$(dirname "$FULL_PATH")
+
+# 3. Extract the filename
+SCRIPT_NAME=$(basename "$FULL_PATH")
+
+# --- Output tests (can be removed) ---
+#echo "Full path:          $FULL_PATH"
+#echo "Directory:          $SCRIPT_PATH"
+#echo "Script name:        $SCRIPT_NAME"
+
+VENVDIR="${SCRIPT_PATH}/.venv"
+#echo ${VENVDIR}
+
+# If ${VENVDIR} does not exist, create it
+if ! [ -d "${VENVDIR}/" ]; then
+  echo -n "Create virtual environment ..."
+  mkdir ${VENVDIR}/
+  virtualenv --python=python3 ${VENVDIR}/ >/dev/null
+  echo "done."
+
+  # Install Python requirements
+  source ${VENVDIR}/bin/activate
+
+  echo -n "Install python requirements ..."
+  ${VENVDIR}/bin/pip install -r "${SCRIPT_PATH}/requirements.txt" >/dev/null
+  echo "done." 
+else
+  # echo "Virtual environment found."
+  source ${VENVDIR}/bin/activate
+fi
+
+# Execute the application from src/aibar/aibar:
+PYTHONPATH="${SCRIPT_PATH}/src/aibar:${PYTHONPATH}" \
+    exec ${VENVDIR}/bin/python3 -m aibar.cli "$@"
